7977f0cbd61dcb27b365a7779980f3f942c5ef98
Trygve Lie <post@trygve-lie.com>
2026-09-25 12:07:18 +0200

    Use full frame

--- templates/document.js
+++ templates/document.js
@@ -20,7 +20,7 @@ export function document(title, body, opts = {}) {
 <html lang="en" class="wa-theme-default wa-cloak">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="default" />
